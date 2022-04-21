89090d2cf068e60081628d5613f7395e068ec55e
Repollo1000 <>
2022-04-21 15:29:34 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -37,13 +37,14 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
+    
     //new->lower_than = lower_than;
     return NULL;
 }
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
+
 
 }
 
@@ -70,6 +71,19 @@ void eraseTreeMap(TreeMap * tree, void* key){
 
 
 Pair * searchTreeMap(TreeMap * tree, void* key) {
+    TreeNode* aux = tree->root;
+  while(aux != NULL)
+  {
+    if(tree->lower_than(key , aux->pair->key)==1)
+      aux = aux->left;
+    else if(tree->lower_than(aux->pair->key)==1)
+      aux = aux->right;
+    else
+    {
+      tree->current = aux;
+      return aux->pair;
+    }
+  }
     return NULL;
 }
 
